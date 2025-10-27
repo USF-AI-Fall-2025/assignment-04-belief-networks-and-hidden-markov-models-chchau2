a74df4e5bf174f9608bb981e1eaa1e7b678b7138
chchau2 <chloeychau@gmail.com>
2025-10-27 16:06:39 -0700

    adding finished assignment adn readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Belief-Networks-Hidden-Markov-Models
 Fall 2025 CS 362/562
+
+1) Give an example of a word which was correctly spelled by the user, but which was incorrectly “corrected” by the algorithm. Why did this happen?
+
+When I inputted ‘nevada’, the program gave me ‘Nevade’. This is likely due to the algorithm calculating that given that I typed ‘a’, it is still more likely that I meant to type ‘e’, not ‘a’. 
+Before my loop in the main method, I printed out the probabilities for ‘d’ and got this output: Sample emission probs for 'd': {'d': 0.7692307692307693, 'a': 0.02197802197802198, 'e': 0.10989010989010989, 'i': 0.03296703296703297, ' ': 0.01098901098901099, 'f': 0.01098901098901099, 't': 0.01098901098901099, 'g': 0.01098901098901099, 'r': 0.02197802197802198} 
+The probability for ‘e’ is about 0.11 and is much higher than ‘a’, which was about 0.02. 
+
+2) Give an example of a word which was incorrectly spelled by the user, but which was still incorrectly “corrected” by the algorithm. Why did this happen?
+
+When I entered ‘transistion’, it incorrectly “corrected” to transistion (no change).
+Looking at the training data, the sequence of letters, s->i->t is slightly less common than s->i->s. And more specifically, n->s->i->t is less common than n->s->i->s. Because of this, the algorithm kept the spelling for the more likely version, which was the original, incorrect form.
+
+
+3) Give an example of a word which was incorrectly spelled by the user, and was correctly corrected by the algorithm. Why was this one correctly corrected, while the previous two were not?
+
+One word that was incorrectly spelled was ‘directli’, and it was correctly corrected by the algorithm to ‘directly’. The algorithm correctly thought that a ‘y’ was meant to be typed instead of ‘i’. This one was likely correctly corrected because there are a lot of instances in the training dataset where ‘ly’ is the correct spelling, and not so many instances where ‘li’ is a correct instance. 
+
+4) How might the overall algorithm’s performance differ in the “real world” if that training dataset is taken from real typos collected from the internet, versus synthetic typos (programmatically generated)?
+
+If the training dataset came from real typos from the internet, the results of the algorithm would most likely be a lot more accurate. It would accurately capture the common mistakes people make when typing, instead of fabricated ones. Synthetic typos wouldn’t really reflect how people actually mistype words, so an algorithm trained on fake typos would not be nearly as accurate as one trained on real typing behavior. 
--- alarm.py
+++ alarm.py
@@ -1,7 +1,7 @@
-from pgmpy.models import BayesianNetwork
+from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.inference import VariableElimination
 
-alarm_model = BayesianNetwork(
+alarm_model = DiscreteBayesianNetwork(
     [
         ("Burglary", "Alarm"),
         ("Earthquake", "Alarm"),
@@ -15,7 +15,7 @@ from pgmpy.factors.discrete import TabularCPD
 
 cpd_burglary = TabularCPD(
     variable="Burglary", variable_card=2, values=[[0.999], [0.001]],
-    state_names={"Burglary":['no','yes']},
+    state_names={"Burglary":["no","yes"]},
 )
 cpd_earthquake = TabularCPD(
     variable="Earthquake", variable_card=2, values=[[0.998], [0.002]],
@@ -27,7 +27,7 @@ cpd_alarm = TabularCPD(
     values=[[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
     evidence=["Burglary", "Earthquake"],
     evidence_card=[2, 2],
-    state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['yes','no']},
+    state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['no','yes']},
 )
 cpd_johncalls = TabularCPD(
     variable="JohnCalls",
@@ -35,7 +35,7 @@ cpd_johncalls = TabularCPD(
     values=[[0.95, 0.1], [0.05, 0.9]],
     evidence=["Alarm"],
     evidence_card=[2],
-    state_names={"Alarm":['yes','no'], "JohnCalls":['yes', 'no']},
+    state_names={"Alarm":['no','yes'], "JohnCalls":['no', 'yes']},
 )
 cpd_marycalls = TabularCPD(
     variable="MaryCalls",
@@ -43,18 +43,29 @@ cpd_marycalls = TabularCPD(
     values=[[0.1, 0.7], [0.9, 0.3]],
     evidence=["Alarm"],
     evidence_card=[2],
-state_names={"Alarm":['yes','no'], "MaryCalls":['yes', 'no']},
+    state_names={"Alarm":['no','yes'], "MaryCalls":['no', 'yes']},
 )
 
-# Associating the parameters with the model structure
-alarm_model.add_cpds(
-    cpd_burglary, cpd_earthquake, cpd_alarm, cpd_johncalls, cpd_marycalls)
+def main():
+    # Associating the parameters with the model structure
+    alarm_model.add_cpds(
+        cpd_burglary, cpd_earthquake, cpd_alarm, cpd_johncalls, cpd_marycalls)
 
-alarm_infer = VariableElimination(alarm_model)
+    alarm_infer = VariableElimination(alarm_model)
 
-#print(alarm_infer.query(variables=["JohnCalls"],evidence={"Earthquake":"yes"}))
-#
-#the probability of Mary Calling given that John called
+    print(alarm_infer.query(variables=["JohnCalls"],evidence={"Earthquake":"yes"}))
+    #
+    #the probability of Mary Calling given that John called
+    print(alarm_infer.query(variables=["MaryCalls"],evidence={"JohnCalls":"yes"}))
 
-q = alarm_infer.query(variables=["Alarm", "Burglary"],evidence={"MaryCalls":"yes"})
-print(q)
+    #the probability of both John and Mary calling given Alarm
+    print(alarm_infer.query(variables=["JohnCalls","MaryCalls"],evidence={"Alarm":"yes"}))
+
+    #The probability of Alarm, given that Mary called
+    print(alarm_infer.query(variables=["Alarm"],evidence={"MaryCalls":"yes"}))
+
+    q = alarm_infer.query(variables=["Alarm", "Burglary"],evidence={"MaryCalls":"yes"})
+    print(q)
+
+if __name__ == "__main__":
+    main()
